bcd5fa38d1f0a56f6d8fad22786c4afd67564207
Lucas <lucas.rmo2017@gmail.com>
2024-06-11 19:22:44 -0300

    pastas de templates criadas e configuração inicial dos models,urls e admin

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50,blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(blank=True)
+    created_date = models.DateField(default = timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.first_name
