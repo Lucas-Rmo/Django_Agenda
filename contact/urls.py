7f9aefef7370f94eab4faef06081ce944ae9b1c0
Lucas <lucas.rmo2017@gmail.com>
2024-06-18 19:18:04 -0300

    Criação de paginação,nova view para criação e contato, alteração nas urls para o CRUD, inicio do form para criação de contato.

--- contact/urls.py
+++ contact/urls.py
@@ -5,6 +5,12 @@ from contact import views
 app_name = "contact"
 urlpatterns = [
     path("",views.index,name="index"),
-    path("<int:contact_id>",views.contact,name="contact"),
-    path("search/",views.search,name="search" )
+    path("search/",views.search,name="search" ),
+
+
+    #crud
+    path("contact/<int:contact_id>/detail/",views.contact,name="contact"),
+    path("contact/create/",views.create,name="create"),
+    path("contact/<int:contact_id>/update/",views.contact,name="contact"),
+    path("contact/<int:contact_id>/delete/",views.contact,name="contact"),
 ]
