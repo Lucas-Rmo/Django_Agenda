2dc2862515d2e101f9becd823c2811400e34f4bf
Lucas <lucas.rmo2017@gmail.com>
2024-06-25 19:47:10 -0300

    Alteração do form de criação de usuário e autenticação adicionada.

--- contact/forms.py
+++ contact/forms.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django import forms
 from . import models
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 class ContactForm(forms.ModelForm):
     picture = forms.ImageField(
         widget = forms.FileInput(
@@ -72,4 +72,30 @@ class ContactForm(forms.ModelForm):
     
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail', code='invalid')
+            )
+
+        return email
